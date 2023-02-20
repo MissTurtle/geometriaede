b8806d9e7ebf53793ad3307662e7e7abd3f1a3ce
Elena <elenagarciadiaz@hotmail.com>
2023-02-20 12:55:06 +0100

    versión refactorizada

--- src/FiguraGeometrica_EGD.java
+++ src/FiguraGeometrica_EGD.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_EGD {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_EGD(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
