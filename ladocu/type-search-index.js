b5c21318ab8d7a919042cbecf89a38694abd5392
Elena <elenagarciadiaz@hotmail.com>
2023-02-20 13:33:16 +0100

    Documentar es de sabios, aunque leerla sea pesado

---
+++ ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_EGD"},{"p":"<Unnamed>","l":"FiguraGeometrica_EGD"},{"p":"<Unnamed>","l":"Rectangulo_EGD"},{"p":"<Unnamed>","l":"Triangulo_EGD"}];updateSearchResults();
